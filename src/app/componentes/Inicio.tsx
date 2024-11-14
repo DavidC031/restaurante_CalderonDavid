00ae1bc8500fe473d9f2bda51f7146a51c879ebd
dcalderon5 <dcalderon5@cuc.edu.co>
2024-11-13 22:13:55 -0500

    se actualizo inicio

--- src/app/componentes/Inicio.tsx
+++ src/app/componentes/Inicio.tsx
@@ -1,24 +1,21 @@
 export const Inicio = () => {
   return (
     <>
-     <div className="pt -8 d-flex justify-content-center">
-     <div className="col -md-6">
-        <div className="h-100 p-5 text-bg-dark rounded-3">
-        <h2>Change the background</h2>
-        <p>
-          Swap the background-color utility and add a .text-* color utility to
-          mix up the jumbotron look. Then, mix and match with additional
-          component themes and more.
-        </p>
-        <button className="btn btn-outline-light" type="button">
-          Example button
-        </button>
-      </div> 
-      </div>   
-     
-     </div>
-      
-      
+      <div className="pt -8 d-flex justify-content-center">
+        <div className="col -md-6">
+          <div className="h-100 p-5 text-bg-dark rounded-3">
+            <h2>Bienvenidos a Sabor Colombiano</h2>
+            <p>
+              Disfruta de la auténtica comida tradicional colombiana. Desde
+              arepas hasta bandeja paisa, cada plato es una celebración de
+              nuestros sabores.
+            </p>
+            <button className="btn btn-outline-light" type="button">
+              Ver menu
+            </button>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
