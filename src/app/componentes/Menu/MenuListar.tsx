26fdc40a1cfbec4f0ad9f017f274b70d2d415cf6
dcalderon5 <dcalderon5@cuc.edu.co>
2024-11-14 12:20:29 -0500

    Crud Menu listar

--- src/app/componentes/Menu/MenuListar.tsx
+++ src/app/componentes/Menu/MenuListar.tsx
@@ -1,4 +1,17 @@
+import { useState } from "react";
+import { ARREGLO_PLATOS } from "../../mocks/Plato-mocks";
+import { Plato } from "../../modelos/Plato";
+import { ARREGLO_PLATOS_REGION } from "../../utilidades/dominios/DomRegion";
+
 export const MenuListar = () => {
+  const [arrPlatos] = useState<Plato[]>(ARREGLO_PLATOS);
+  const obtenerNombre =(valor: string)=>{
+    for(const objRegion of ARREGLO_PLATOS_REGION){
+    if(objRegion.codRegion==valor){
+      return objRegion.nombreRegion;
+    } 
+    }
+  }
   return (
     <>
     <div className="pt-5 d-flex justify-content-center">
@@ -8,33 +21,31 @@ export const MenuListar = () => {
           <tr>
             <th style={{width:"20%"}}>No.</th>
             <th style={{width:"30%"}}>Nombre de Plato</th>
-            <th style={{width:"20%"}}>Acompañamiento</th>
-            <th style={{width:"25%"}} className="text-center">Precio</th>
-            <th style={{width:"25%"}}>Región</th>
+            <th style={{width:"20%"}}>Precio</th>
+            <th style={{width:"25%"}} className="text-center">Región</th>
+            <th style={{width:"25%"}}>Imagen</th>
+            
+
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td >1</td>
-            <td>Bandeja Paisa</td>
-            <td>Con aguacate</td>
-            <td>35000</td>
-            <td>Paisa</td>
+          {arrPlatos.map((miPla: Plato)=>(
+            <tr>
+            <td >{miPla.codPlato}</td>
+            <td>{miPla.nombrePlato}</td>
+            <td>{miPla.precioPlato}</td>
+            <td className="text-center">{obtenerNombre(miPla.regionPlato)}</td>
+            <td>
+              <img src={miPla.imagenPlatoBase64} alt={miPla.nombrePlato} className="imagenListado"/> 
+              <br/>
+              {miPla.imagenPlato}
+              </td>
           </tr>
-          <tr>
-            <td>2</td>
-            <td>Mute Santendereano</td>
-            <td>Con Arroz</td>
-            <td>25000</td>
-            <td>Andina</td>
-          </tr>
-          <tr>
-            <td>3</td>
-            <td>Mojarra</td>
-            <td>Con Arroz</td>
-            <td>15000</td>
-            <td>Costa</td>
-          </tr>
+          ))}
+          
+          
+
+         
         </tbody>
       </table>
     </div>
